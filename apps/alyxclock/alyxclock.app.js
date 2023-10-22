0cb1ea100f81ad4f7c9c65e5e8f3c489f3561230
Jaeth87 <78368646+Jaeth87@users.noreply.github.com>
2023-10-22 14:22:42 +0200

    Update alyxclock.app.js

--- apps/alyxclock/alyxclock.app.js
+++ apps/alyxclock/alyxclock.app.js
@@ -42,33 +42,33 @@ function icon (icon, x, y, size, gap) {
 }
 
 function ohmA(x, y) {
-  g.setColor('#666');
+  g.setColor('#ff00fb');
   g.fillRect(x, y, x+8, y+15);
-  g.setColor('#00f');
+  g.setColor('#ff00fb');
   g.drawLine(x, y + 4, x + 8, y + 4);
-  g.setColor('#f00');
+  g.setColor('#ff00fb');
   g.drawLine(x, y + 6, x + 8, y + 6);
-  g.setColor('#0f0');
+  g.setColor('#ff00fb');
   g.drawLine(x, y + 8, x + 8, y + 8);
 }
 
 function ohmB(x, y) {
-  g.setColor('#666');
+  g.setColor('#ff00fb');
   g.fillRect(x, y, x+15, y+8);
-  g.setColor('#00f');
+  g.setColor('#ff00fb');
   g.drawLine(x + 4, y + 8, x + 4, y);
-  g.setColor('#f00');
+  g.setColor('#ff00fb');
   g.drawLine(x + 6, y + 8, x + 6, y);
-  g.setColor('#0f0');
+  g.setColor('#ff00fb');
   g.drawLine(x + 8, y + 8, x + 8, y);
 }
 
 function heart (x, y) {
-  g.setColor('#000');
+  g.setColor('#ff00fb');
   g.fillRect(x-2, y-2, x + 32, y + 32)
-  g.setColor('#fff200');
+  g.setColor('#ff00fb');
   g.drawRect(x-2, y-2, x + 32, y + 32)
-  g.setColor('#f00');
+  g.setColor('#ff00fb');
   icon(icoH, x, y, 4, 2);
 }
 
@@ -81,9 +81,9 @@ function resin() {
   const resinPosY = 130;
   g.setColor('#000');
   g.fillRect(resinPosX - 3, resinPosY - 3, Bangle.appRect.w - resinPosX + 2, resinPosY + 40);
-  g.setColor('#666');
+  g.setColor('#ff00fb');
   g.drawRect(resinPosX - 3, resinPosY - 3, Bangle.appRect.w - resinPosX + 2, resinPosY + 40);
-  g.setColor('#6ff');
+  g.setColor('#ff00fb');
   icon(icoR, resinPosX, resinPosY, 3, 1);
   g.setFont('6x8', 5);
   g.setFontAlign(-1, -1);
@@ -95,29 +95,29 @@ function resin() {
 }
 
 function screw(x, y) {
-  g.setColor('#666').fillCircle(x, y, 4).setColor('#000').drawLine(x - 4, y, x + 4, y)
+  g.setColor('#ff00fb').fillCircle(x, y, 4).setColor('#000').drawLine(x - 4, y, x + 4, y)
 }
 
 function led(x,y) {
-  g.setColor('#0f0').fillCircle(x, y, 8).setColor('#fff').fillCircle(x-3, y-3, 3);
+  g.setColor('#ff00fb').fillCircle(x, y, 8).setColor('#fff').fillCircle(x-3, y-3, 3);
 }
 
 
 function drawTime() {
   const R = Bangle.appRect;
-  g.setBgColor('#000');
+  g.setBgColor('#ff00fb');
   g.clear();
   Bangle.drawWidgets();
   g.reset();
 
   // pcb
-  g.setColor('#030').fillRect(R.x, R.y, R.x2, R.y2);
+  g.setColor('#ff00fb').fillRect(R.x, R.y, R.x2, R.y2);
   screw(R.x + 8, R.y + 8)
   screw(R.x2 - 8, R.y + 8)
   screw(R.x + 8, R.y2 - 8)
   screw(R.x2 - 8, R.y2 - 8)
   for(let i=0; i<6; i++) {
-    g.setColor('#fff');
+    g.setColor('#ff00fb');
     g.drawLine(24 + i * 9, 70, 24 + i * 9, 110);
     g.drawLine(24 + i * 9, 110, 54 + i * 9, 140);
   }
@@ -138,16 +138,16 @@ function drawTime() {
 
 
 
-  g.setColor('#666');
+  g.setColor('#ff00fb');
   for (let i=0; i<6; i++) {
     g.fillCircle(110 + i*10, 80+10, 3);
     g.fillCircle(110 + i*10, 110+10, 3);
   }
-  g.setColor('#000');
+  g.setColor('#ff00fb');
   g.fillRect(110, 80+10, 170, 110+10);
-  g.setColor('#666');
+  g.setColor('#ff00fb');
   g.drawRect(110, 80+10, 170, 110+10);
-  g.setFont('6x8').setColor('#666').drawString('AH-118080\n0WT 18-001', 112, 85+10);
+  g.setFont('6x8').setColor('#ff00fb').drawString('AH-118080\n0WT 18-001', 112, 85+10);
 
 
   resin();
